9556df37b9106bc397cdb19c401f1e9171beefa3
VidadiMurad <vidad0murad@gmail.com>
2026-04-19 14:51:52 +0400

    Add title

--- App.java
+++ App.java
@@ -7,6 +7,18 @@ import java.util.Locale;
 
 public class App {
     public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+    scanner.useLocale(Locale.US);
+     
+    // Project title
+    System.out.println("---------------------------------");
+    System.out.println("Welcome to Number Guessing Game!");
 
+
+
+
+
+    scanner.close();
     }
 }

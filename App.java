cb6c36b9386811245ec74269d2cd390fe95480b2
VidadiMurad <vidad0murad@gmail.com>
2026-04-19 14:46:49 +0400

    Add library for project

--- App.java
+++ App.java
@@ -1,3 +1,12 @@
+// Number Guessing Game 
+
+// Libraries
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Locale;
+
 public class App {
-    
+    public static void main(String[] args){
+
+    }
 }

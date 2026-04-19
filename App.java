88098b3fba3af6c5fef74892b5857d23d33469ea
VidadiMurad <vidad0murad@gmail.com>
2026-04-19 14:53:58 +0400

    Add Variables

--- App.java
+++ App.java
@@ -10,10 +10,23 @@ public class App {
     Scanner scanner = new Scanner(System.in);
     Random random = new Random();
     scanner.useLocale(Locale.US);
+
+
+    // Variables 
+
+    int max = 100;
+    int min = 1;
+    int randomNum = random.nextInt(min, max+ 1);
+
      
     // Project title
     System.out.println("---------------------------------");
     System.out.println("Welcome to Number Guessing Game!");
+
+    
+    System.out.print("Enter Number between 1-100");
+    
+
 
 
 
